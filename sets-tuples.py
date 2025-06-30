43169363fe8a27822f3962c33bdf23998a64b8f6
ben-tsai-kou <97225493+ben-tsai-kou@users.noreply.github.com>
2025-06-30 16:43:28 +0900

    sets and tuples

---
+++ sets-tuples.py
@@ -0,0 +1,21 @@
+# my_set = {1,2,3,4,5}
+# print(my_set)
+# # print(my_set[0]) error
+
+# my_set.discard(3) # {1, 2, 4, 5}
+# print(my_set)
+
+# my_set.clear()
+# print(my_set) # nothing
+
+# my_set.add(6)
+# print(my_set) # {6}
+
+# my_set.update([7,8])
+# print(my_set) # {6, 7, 8}
+
+my_tuple = (1,2,3,4,5)
+print(my_tuple) # (1, 2, 3, 4, 5)
+print(len(my_tuple)) # 5
+print(my_tuple[0]) # 1
+
