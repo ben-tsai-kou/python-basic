94380c9faf47b647db6c06e8823ba20af2357c3a
ben-tsai-kou <97225493+ben-tsai-kou@users.noreply.github.com>
2025-07-01 12:12:15 +0900

    OOP in Python

---
+++ oop/enemy.py
@@ -0,0 +1,4 @@
+class Enemy:
+    type_of_enemy: str
+    health_points: int = 10
+    attack_power: int = 1
---
+++ oop/main.py
@@ -0,0 +1,8 @@
+from enemy import Enemy
+
+enemy1 = Enemy()
+enemy2 = Enemy()
+enemy2.health_points = 20
+
+print(enemy1.health_points)
+print(enemy2.health_points)
