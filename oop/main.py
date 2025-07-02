ac5a8f0f1280f6c995f8997afb4cc57a288db4a0
ben-tsai-kou <97225493+ben-tsai-kou@users.noreply.github.com>
2025-07-02 12:49:29 +0900

    composition

--- oop/main.py
+++ oop/main.py
@@ -1,6 +1,8 @@
 from ogre import Ogre
 from zombie import Zombie
 from enemy import Enemy
+from hero import Hero
+from weapon import Weapon
 
 
 def battle(e1: Enemy, e2: Enemy):
@@ -30,7 +32,35 @@ def battle(e1: Enemy, e2: Enemy):
         print(f"{e2.get_type_of_enemy()} wins!")
 
 
+def hero_battle(hero: Hero, enemy: Enemy):
+    while hero.health_points > 0 and enemy.health_points > 0:
+        print("--------------------------------")
+
+        enemy.special_attack()
+
+        print(f"Hero: {hero.health_points}: {hero.health_points} HP left")
+        print(
+            f"{enemy.get_type_of_enemy()}: {enemy.health_points}: {enemy.health_points} HP left"
+        )
+        enemy.attack()
+        hero.health_points -= enemy.attack_damage
+        hero.attack()
+        enemy.health_points -= hero.attack_damage
+
+    print("--------------------------------")
+
+    if hero.health_points > 0:
+        print("Hero wins!")
+    else:
+        print(f"{enemy.get_type_of_enemy()} wins!")
+
+
 zombie = Zombie(10, 1)
 ogre = Ogre(20, 3)
 
-battle(zombie, ogre)
+hero = Hero(10, 1)
+weapon = Weapon("Sword", 10)
+hero.weapon = weapon
+hero.equip_weapon()
+
+hero_battle(hero, ogre)
